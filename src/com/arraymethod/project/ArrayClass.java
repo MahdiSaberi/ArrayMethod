297d460bc339e3958db107d08381792a55528446
mahdisaberi <mahdisaberi35@gmail.com>
2022-05-29 15:42:49 +0430

    Array Class Created

---
+++ src/com/arraymethod/project/ArrayClass.java
@@ -0,0 +1,10 @@
+package com.arraymethod.project;
+
+import java.util.Iterator;
+
+public class ArrayClass implements Iterable{
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
