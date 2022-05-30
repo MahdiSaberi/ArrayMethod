8ed2264833756d7febbd53bfd9f664c3bd21c410
mahdisaberi <mahdisaberi35@gmail.com>
2022-05-31 02:21:13 +0430

    Tried to Work with Iterator!

--- src/com/arraymethod/project/ArrayMethod.java
+++ src/com/arraymethod/project/ArrayMethod.java
@@ -1,8 +1,8 @@
 package com.arraymethod.project;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
+
 
 public class ArrayMethod{
 
@@ -12,23 +12,47 @@ public class ArrayMethod{
         if(numbers.size() % 2 == 1)
             numbers.remove(numbers.size()-1);
         //
-        int size = numbers.size();
 
-        for(int i = 0;i < size; i++){
+        Iterator it = numbers.iterator();
+        int index = 0;
 
-            if(i % 2 == 0){
-                if(numbers.get(i) > numbers.get(i+1)){
-                    numbers.remove(i);
-                    numbers.remove(i);
-                    numbers.add(i,-999);
-                    numbers.add(i,-999);
+        while(it.hasNext()){
+
+                if (index % 2 == 0) {
+                    if(numbers.size() > index){
+
+                        if (numbers.get(index) > numbers.get(index + 1)) {
+
+                            numbers.remove(index);
+                            numbers.remove(index);
+                            it = numbers.iterator();
+                            index = 0;
+                        }
+
+                    }
+
                 }
+
+                index++;
+                it.next();
+
             }
+        //int size = numbers.size();
 
-        }
-        numbers.removeAll(Collections.singleton(-999));
+        //for(int i = 0;i < size; i++){
+
+          //  if(i % 2 == 0){
+            //    if(numbers.get(i) > numbers.get(i+1)){
+              //      numbers.remove(i);
+                //    numbers.remove(i);
+                  //  numbers.add(i,-999);
+                    //numbers.add(i,-999);
+                //}
+            //}
+        //}
+
+        //numbers.removeAll(Collections.singleton(-999));
         System.out.println(numbers);
-
     }
 
 }
