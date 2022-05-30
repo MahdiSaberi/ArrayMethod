d1cbe910d1144b8fc772f3b18f7b21aaa02be55a
mahdisaberi <mahdisaberi35@gmail.com>
2022-05-30 16:16:52 +0430

    ArrayNumber.java Added

---
+++ src/com/arraymethod/project/Test.java
@@ -0,0 +1,15 @@
+package com.arraymethod.project;
+
+import java.util.ArrayList;
+
+public abstract class Test {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<>();
+        number.add(1);
+        number.add(2);
+        number.add(5);
+        number.add(4);
+
+        ArrayNumber.getArrayList(number);
+    }
+}
